c74663965c0d8421f4f302b3e328c080e1f1b398
Guillaume <guillaume@wechantloup.com>
2026-04-22 19:59:13 +0200

    Add comments to Memory

--- composeApp/src/commonMain/kotlin/com/wechantloup/gameboykmp/memory/Memory.kt
+++ composeApp/src/commonMain/kotlin/com/wechantloup/gameboykmp/memory/Memory.kt
@@ -1,5 +1,22 @@
 package com.wechantloup.gameboykmp.memory
 
+/**
+ * Represents the Game Boy memory bus - the full 64KB addressable space.
+ *
+ * Memory map:
+ * 0x0000 - 0x3FFF  ROM Bank 0 (cartridge, fixed)
+ * 0x4000 - 0x7FFF  ROM Bank N (cartridge, switchable via MBC)
+ * 0x8000 - 0x9FFF  VRAM (Video RAM)
+ * 0xA000 - 0xBFFF  External RAM (cartridge)
+ * 0xC000 - 0xCFFF  Work RAM Bank 0
+ * 0xD000 - 0xDFFF  Work RAM Bank 1
+ * 0xE000 - 0xFDFF  Echo RAM (mirror of 0xC000-0xDDFF, avoid using)
+ * 0xFE00 - 0xFE9F  OAM (Sprite Attribute Table)
+ * 0xFEA0 - 0xFEFF  Unusable
+ * 0xFF00 - 0xFF7F  I/O Registers
+ * 0xFF80 - 0xFFFE  High RAM (HRAM)
+ * 0xFFFF           Interrupt Enable Register
+ */
 class Memory {
     private val memory = IntArray(0x10000)
 
